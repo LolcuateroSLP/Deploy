947fc96734fbe2d3c04669627a1030406d3343b7
edwin lopez <edwinslp16@gmail.com>
2023-09-03 05:40:48 -0600

    ca

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/changeLang',function(){
     $locale = session()->get('locale');
-    // dd("there");
+    dd($locale);
     if($locale == "es"){
         App::setLocale('en');
         session()->put('locale', "en");
