fadbb3b89d30d31ef6e8ed05c9653abb4edfc666
edwin lopez <edwinslp16@gmail.com>
2023-09-24 07:54:57 -0600

    quitar dd

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/changeLang',function(){
     $locale = session()->get('locale');
-    dd($locale);
     if($locale == "es"){
         App::setLocale('en');
         session()->put('locale', "en");
